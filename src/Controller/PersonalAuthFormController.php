3077d509a4bc04062a14b606e228e8219f43fd76
github-ci <you@example.com>
2025-06-11 00:02:30 +0800

    修正过时用法

--- src/Controller/PersonalAuthFormController.php
+++ src/Controller/PersonalAuthFormController.php
@@ -5,7 +5,7 @@ namespace Tourze\RealNameAuthenticationBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Tourze\RealNameAuthenticationBundle\Enum\AuthenticationMethod;
 use Tourze\RealNameAuthenticationBundle\Service\PersonalAuthenticationService;
 
--- src/Controller/PersonalAuthenticationController.php
+++ src/Controller/PersonalAuthenticationController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Tourze\RealNameAuthenticationBundle\Dto\PersonalAuthDto;
